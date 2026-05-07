2be4ffdd268c434c5412d495ba751fa4b67e686d
tonycho999 <83383706+tonycho999@users.noreply.github.com>
2026-05-07 17:08:40 +0800

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,9 +5,9 @@ export default function Header() {
     <header className="py-6 mb-10 border-b border-gray-200">
       <div className="container mx-auto flex justify-between items-center px-4">
         {/* 블로그 로고는 SEO를 위해 남겨두되, 다른 링크는 제거합니다. */}
-        <Link href="/" className="text-2xl font-bold tracking-tight text-gray-900">
-          Product Review
-        </Link>
+      <Link href="/" className="text-2xl font-bold tracking-tight text-transparent bg-clip-text bg-gradient-to-r from-red-600 to-orange-500">
+        Product Review
+      </Link>
         
         {/* 네비게이션 메뉴 삭제됨 */}
       </div>
